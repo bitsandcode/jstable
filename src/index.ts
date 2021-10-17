c5ac28974640000674db180d421478199365ffae
Benjamin Kalungi <benkalungi256@gmail.com>
2021-10-17 11:43:07 +0000

    update: Basic structure for our code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+
+export default class Table {
+       columns: number;
+       rows: number;
+
+       constructor(columns : number, rows: number){
+          this.columns = columns;
+          this.rows = rows;
+       }
+
+       
+}
